3378e9edb60f7e014d84aa47fca82db072d95ca7
antonhristozov <anton.hristozov@gmail.com>
2020-12-01 14:46:15 +0000

    Had to change the path type for this version of Python on this Raspbian image

--- Line_Follower_Test.py
+++ Line_Follower_Test.py
@@ -1,11 +1,13 @@
 # ctypes_test.py
 import ctypes
 import pathlib
+import os 
 
 if __name__ == "__main__":
     print("Line Follower Python Test Application Running")
     # Load the shared library into ctypes
-    libname = pathlib.Path().absolute() / "libLine_Follower.so"
+    libname = os.path.abspath(".") + "/" + "libLine_Follower.so";
+    print(libname)
     c_lib = ctypes.CDLL(libname)
     dt_list = c_lib.read_digital()
     ptr = ctypes.cast(dt_list,ctypes.POINTER(ctypes.c_int))
